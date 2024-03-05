d957edb0a6cc6998251959699a97a5a012d1b183
0xRoy <1997roylee@gmail.com>
2024-03-05 19:30:28 +0800

    update

--- src/app/components/DisplayBlockNumber.tsx
+++ src/app/components/DisplayBlockNumber.tsx
@@ -5,15 +5,19 @@ import { useEffect, useState } from "react";
 
 export default function DisplayBlockNumber() {
   const [blockNumber, setBlockNumber] = useState<number>(0);
-  const provider = new ethers.JsonRpcProvider(
-    "https://rpc.ankr.com/blast/6c566b719e46d450ac87294a9ad8d45a88b0f3d9acf2763053333355589f6cf2"
-  );
 
   useEffect(() => {
-    provider.getBlockNumber().then((blockNumber) => {
-      setBlockNumber(blockNumber);
-    });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    const provider = new ethers.JsonRpcProvider(
+      "https://rpc.ankr.com/blast/6c566b719e46d450ac87294a9ad8d45a88b0f3d9acf2763053333355589f6cf2"
+    );
+
+    const interval = setInterval(async () => {
+      const number = await provider.getBlockNumber();
+      setBlockNumber(number);
+    }, 60 * 60);
+
+    // Clear interval on component unmount
+    return () => clearInterval(interval);
   }, []);
 
   return <div>Block Number: {blockNumber}</div>;
